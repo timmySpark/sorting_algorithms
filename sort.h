b895151fb0993c49f4ac21ec804da7b03157d5c4
Wonders Victor <wondersprince1@gmail.com>
2023-11-21 22:22:23 +0100

    Added sort prototype

--- sort.h
+++ sort.h
@@ -39,6 +39,7 @@ void merge_helper(int *array, int low, int high);
 int get_max(int *arr, int num);
 void radix_counting_sort(int *array, size_t size, int sig, int *buff);
 void radix_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 
 #endif /* SORT_H */
