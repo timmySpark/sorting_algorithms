2503b53acf1e05a8c8ab06a457967c36b8bacc9c
timmySpark <timmyspark1@gmail.com>
2023-11-21 01:21:26 -0800

    [feat] :- added header files to handle task prototypes

---
+++ sort.h
@@ -0,0 +1,44 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+void swap_num(int *a, int *b);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+int lumoto_partition(int *a, int low, int high, size_t size);
+void quick_helper(int *a, int low, int high, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge(int *array, int low, int mid, int high);
+void merge_sort(int *array, size_t size);
+void merge_helper(int *array, int low, int high);
+int get_max(int *arr, int num);
+void radix_counting_sort(int *array, size_t size, int sig, int *buff);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+
+#endif /* SORT_H */
