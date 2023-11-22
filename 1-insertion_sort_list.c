1f5322815026ba9137aefeebbe06dbd4db013159
Wonders Victor <wondersprince1@gmail.com>
2023-11-22 10:45:13 +0100

    Corrections in insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,12 +11,13 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *head = (*list)->next;
+	listint_t *head;
 	listint_t  *current, *temp;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
+	head = (*list)->next;
 	while (head)
 	{
 		current = head;
